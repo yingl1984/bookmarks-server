d0facaa2ae0084ab6e2bd2fc857fd8f69af28d32
Ying Liu <yingl1984@gmail.com>
2021-02-22 00:58:33 -0800

    remove logger

--- src/app.js
+++ src/app.js
@@ -17,10 +17,8 @@ app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
   
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      logger.error(`Unauthorized request to path: ${req.path}`);
       return res.status(401).json({ error: 'Unauthorized request' })
     }
-    // move to the next middleware
     next()
   })
 
