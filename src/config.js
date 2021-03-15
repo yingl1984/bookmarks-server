5ad4e3994e9d9b0e46bd1af00713e23ed31b94ef
Ying Liu <yingl1984@gmail.com>
2021-03-15 01:26:29 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin1:lovebaobao040117@localhost/bookmarks',
-}
+}   
